697b6af32b705d94270692440060b8fd4baccd85
HuniHuns <i690367@gmail.com>
2025-04-11 18:36:34 +0900

    Http Servlet Setting

--- http--servlet/src/main/java/depth/httpservlet/servlet/MyServlet.java
+++
@@ -1,9 +0,0 @@
-package depth.httpservlet.servlet;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
-public interface MyServlet {
-    void service(InputStream input, OutputStream output) throws IOException;
-}
